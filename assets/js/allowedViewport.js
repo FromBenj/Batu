ec642aa4fc7a0f77334cd7feb78ef0a0cbe59455
Benjamin TOUBOUL <touboul.benjamin@gmail.com>
2021-09-15 17:18:36 +0200

    footer and header front-end

--- assets/js/allowedViewport.js
+++ assets/js/allowedViewport.js
@@ -1,5 +1,5 @@
 export function viewportManagement(){
-    if (window.innerWidth > 576) {
+    if (window.innerWidth >= 576) {
         console.log('viewport too large')
         location.replace(viewportReplaceUrl)
     }
